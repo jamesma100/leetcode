ad48df8ec9d1d026de75f5bc951225cf6ab03815
James Ma <jamesma100@gmail.com>
2021-05-22 19:15:06 -0500

    add bfs sol to num_islands

--- graph/num_islands.py
+++ graph/num_islands.py
@@ -1,21 +1,59 @@
+# dfs solution
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        def dfs(i, j):
+            if i < 0 or i >= m:
+                return
+            if j < 0 or j >= n:
+                return
+            if grid[i][j] == "x":
+                return
+            if grid[i][j] == "0":
+                return
+            if grid[i][j] == "1":
+                grid[i][j] = "x"
+                dfs(i+1,j)
+                dfs(i-1,j)
+                dfs(i,j+1)
+                dfs(i,j-1) 
         if not grid:
             return
         count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == "1":
-                    self.dfs(grid, i, j)
-                    count += 1
+                    dfs(i,j)
+                    count+=1
         return count
-    
-    def dfs(self, grid, i, j):
-        if i < 0 or i > len(grid)-1 or j < 0 or j > len(grid[0])-1:
+        
+# bfs solution
+class Solution2:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
             return
-        if grid[i][j] != "1":
-            return
-        grid[i][j] = "x"
-        neighbors = [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]
-        for neighbor in neighbors:
-            self.dfs(grid,neighbor[0],neighbor[1])
+        m = len(grid)
+        n = len(grid[0])
+        positions = [(0,1),(0,-1),(1,0),(-1,0)]
+        def bfs(i,j):
+            q = [(i,j)]
+            while q:
+                cur_x, cur_y = q.pop(0)
+                for pos in positions:
+                    new_x, new_y = cur_x+pos[0], cur_y+pos[1]
+                    if new_x < 0 or new_x >= m:
+                        continue
+                    if new_y < 0 or new_y >= n:
+                        continue
+                    if grid[new_x][new_y] != "1":
+                        continue
+                    grid[new_x][new_y] = "x"
+                    q.append((new_x,new_y))
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    bfs(i,j)
+                    count+=1
+        return count
